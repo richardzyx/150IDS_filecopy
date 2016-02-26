f1f38a36e75395472095f7d2b723d88ea0b42d51
Richard Zhang <richard.zhang711@gmail.com>
2016-02-25 22:22:50 -0500

    10:22pm 2/25 first submit

--- fileclient.cpp
+++ fileclient.cpp
@@ -14,7 +14,6 @@
 #include <cstdlib>
 #include <openssl/sha.h>
 
-
 using namespace std;          // for C++ std library
 using namespace C150NETWORK;  // for all the comp150 utilities
 
@@ -28,13 +27,9 @@ string computeSha(string fileName, char * srcDir);
 bool sendResult(bool result, string fileName, C150DgmSocket *sock);
 void checkMessage(ssize_t readlen, char *msg, ssize_t bufferlen);
 
-int 
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	//will use these to read back later
-	//ssize_t readlen;
-	//char incomingMessage[512];
-
+    GRADEME(argc, argv);
 	if (argc != 5) {
 		fprintf(stderr, "Correct syntax is: %s <servername> <networknastiness> <filenastiness> <srcdir>\n", argv[0]);
 		exit(1);
@@ -44,43 +39,60 @@ main(int argc, char *argv[])
 	char incomingMessage[512];
 	DIR *SRC;
 	SRC = opendir(argv[srcDirArg]);
-        struct dirent *sourceFile;
+    struct dirent *sourceFile;
 	int netNastiness = atoi(argv[netNastinessArg]);
         if (SRC == NULL) {
-                fprintf(stderr,"Error opening source directory %s\n",
+            fprintf(stderr,"Error opening source directory %s\n",
                                                 argv[srcDirArg]);
-                exit(8);
+            exit(8);
         }
 	try {
-	
+        // Setting up Socket with nastiness in network
 		C150DgmSocket *sock = new C150NastyDgmSocket(netNastiness);
 		sock -> setServerName(argv[serverArg]);
 		sock -> turnOnTimeouts(3000);
-		while ((sourceFile = readdir(SRC)) != NULL) {
+		
+        // Check to make sure SRC is not NULL
+        while ((sourceFile = readdir(SRC)) != NULL) {
+            // Get the string version of current file name, and send as message
 			string srcName = sourceFile->d_name;
 			const char *msg = srcName.c_str();
-	                if ((strcmp(sourceFile->d_name, ".") == 0) ||
+            if ((strcmp(sourceFile->d_name, ".") == 0) ||
         	            (strcmp(sourceFile->d_name, "..")  == 0 ))
-			continue;
-			sock -> write(msg, strlen(msg) + 1);
-			readlen = sock -> read(incomingMessage,
-						 sizeof(incomingMessage));
-			int i = 0;
-			while(sock->timedout() && i < 5){
+                continue;
+			
+            // +1 for null character
+            sock -> write(msg, strlen(msg) + 1);
+            int i = 0;
+            *GRADING << "File: " << srcName << ", beginning transmission, attempt " 
+                    << i+1 <<endl;
+			readlen = sock -> read(incomingMessage, sizeof(incomingMessage));
+			
+            while(sock->timedout() && i < 5){
 				sock->write(msg, strlen(msg) + 1);
-	                        readlen = sock -> read(incomingMessage,
-                                                 sizeof(incomingMessage));
 				i++;
+                *GRADING << "File: " << srcName << ", beginning transmission, attempt " 
+                    << i+1 <<endl;
+                readlen = sock -> read(incomingMessage, sizeof(incomingMessage));
 			}
 			if( i >= 5){
 				throw C150NetworkException("Server not responding.");
 			}
+            //do if not timedout, and make sure got the right type of message
 			do{
+                //TODO: add check to first stage messages
+                //We assume 0 nastiness or none duplicate packet in the network
+                *GRADING << "File: " << srcName << " transmission complete, waiting for end-to-end check, attempt " 
+                    << i+1 <<endl;
+                //TODO: add unique ID for this transaction for all following interactions
+                //TODO: pass filenastiness
 				checkSha(readlen, incomingMessage,
 					sizeof(incomingMessage), 
 					argv[srcDirArg], sock);
 			        readlen = sock -> read(incomingMessage,
                                                  sizeof(incomingMessage));
+                //TODO: read until we read the expected packet message, separate stages, add flags
+                //TODO: should proceed only when read SHA1 message
 			}while(!sock->timedout());
 
  
@@ -96,13 +108,17 @@ main(int argc, char *argv[])
 	return 0;
 }
 
-void checkSha(ssize_t readlen, char *msg, ssize_t bufferlen, char *srcDir,
-		C150DgmSocket *sock){
+void checkSha(ssize_t readlen, char *msg, ssize_t bufferlen, char *srcDir, 
+                                                        C150DgmSocket *sock){
 
 	checkMessage(readlen, msg, bufferlen);
 	string fileName = "";
 	string sha1 = "";
+
+    //Temporary message protocol: "a.txt/sha1" as / cannot be in file names
+    //Read until it is not file name
 	bool isFileName = true;
+    //Make sure to skip the final null bit
 	for(int i=0; i < readlen - 1; i++){
 		if(msg[i] == '/') {
 			isFileName = false;
@@ -111,12 +127,16 @@ void checkSha(ssize_t readlen, char *msg, ssize_t bufferlen, char *srcDir,
 		if( isFileName ) fileName += msg[i];
 		else sha1 += msg[i];
 	} 
-	cout <<"file + sha: " << fileName << " " << sha1 << '\n';
+	// cout <<"file + sha: " << fileName << " " << sha1 << '\n';
+
+    //TODO: check to make sure the packet does not come from previous transactions
 	string clientSha = computeSha(fileName, srcDir);
 	bool result;
 	if (clientSha == sha1) result = true;
 	else result = false;
 	int j = 0;
+    
+    //Execute end-to-end send result process, retry five times if no ack from server
 	while(!sendResult(result, fileName, sock) && j < 5)
 	{
 		j++; 
@@ -124,72 +144,92 @@ void checkSha(ssize_t readlen, char *msg, ssize_t bufferlen, char *srcDir,
 }
 
 void checkMessage(ssize_t readlen, char *msg, ssize_t bufferlen){
-        if (readlen == 0) {
-                throw C150NetworkException("Unexpected zero length read in client");
-        }
+    if (readlen == 0) {
+            throw C150NetworkException("Unexpected zero length read in client");
+    }
 
-        if (readlen > (int)(bufferlen)) {
-                throw C150NetworkException("Unexpected over length read in client");
-        }
+    if (readlen > (int)(bufferlen)) {
+            throw C150NetworkException("Unexpected over length read in client");
+    }
 
-        if(msg[readlen-1] != '\0') {
-                throw C150NetworkException("Client received message that was not null terminated");
-        }
+    if(msg[readlen-1] != '\0') {
+            throw C150NetworkException("Client received message that was not null terminated");
+    }
 
 }
 
 string computeSha(string fileName, char * srcDir)
 {
-        char sha1hex[40];
-        std::string path = srcDir;
-        if(path[path.length() - 1] != '/') path += '/';
-        ifstream *t;
-        stringstream *buffer;
-        unsigned char obuf[20];
-        t = new ifstream((path+fileName).c_str());
-        buffer = new stringstream;
-        *buffer << t->rdbuf();
-        SHA1((const unsigned char *)buffer->str().c_str(),
-             (buffer->str()).length(), obuf);
-        for (int i = 0; i < 20; i++)
-        {
-                sprintf (sha1hex + (i*2), "%02x", (unsigned int) obuf[i]);
-        }
-        delete t;
-        delete buffer;
-        std::string sha1 = sha1hex;
-        return sha1;
-
+    //TODO: this current version do not use nastyfile due to our previous bug
+    // we will definitely fix in final submission        
+    char sha1hex[40];
+    std::string path = srcDir;
+    //add '/' to end of path if it is not there
+    if(path[path.length() - 1] != '/') path += '/';
+    ifstream *t;
+    stringstream *buffer;
+    unsigned char obuf[20];
+    t = new ifstream((path+fileName).c_str());
+    buffer = new stringstream;
+    *buffer << t->rdbuf();
+    SHA1((const unsigned char *)buffer->str().c_str(),
+         (buffer->str()).length(), obuf);
+    for (int i = 0; i < 20; i++)
+    {
+            sprintf (sha1hex + (i*2), "%02x", (unsigned int) obuf[i]);
+    }
+    delete t;
+    delete buffer;
+    std::string sha1 = sha1hex;
+    return sha1;
 }
 
 bool sendResult(bool result, string fileName, C150DgmSocket *sock)
 {
 	ssize_t readlen;
 	char incomingMessage[512];	
-	string msg = fileName + '/';
+	
+    string msg = fileName + '/';
 	if(result) msg += "SUCCESS";
 	else msg += "FAILURE";
-	sock->write(msg.c_str(), msg.length() + 1);
-        readlen = sock -> read(incomingMessage,
-			        sizeof(incomingMessage));
-        int i = 0;
-        while(sock->timedout() && i < 5){
-      		sock->write(msg.c_str(), msg.length() + 1);
-      		readlen = sock -> read(incomingMessage,
-            			sizeof(incomingMessage));
-                i++;
-        }
-        if( i >= 5){
-       		throw C150NetworkException("Server not responding.");
-        }
+	
+    sock->write(msg.c_str(), msg.length() + 1);
+    int i = 0;
+    if(result) *GRADING << "File: " << fileName << " end-to-end check succeeded, attempt " 
+                    << i+1 <<endl;
+    else *GRADING << "File: " << fileName << " end-to-end check failed, attempt " 
+                    << i+1 <<endl;
+
+    readlen = sock -> read(incomingMessage, sizeof(incomingMessage));
+
+    while(sock->timedout() && i < 5){
+      	sock->write(msg.c_str(), msg.length() + 1);
+        i++;
+
+        if(result) *GRADING << "File: " << fileName << " end-to-end check succeeded, attempt " 
+                    << i+1 <<endl;
+        else *GRADING << "File: " << fileName << " end-to-end check failed, attempt " 
+                    << i+1 <<endl;
+
+      	readlen = sock -> read(incomingMessage,
+            			         sizeof(incomingMessage));
+    }
+
+    if( i >= 5){
+       	throw C150NetworkException("Server not responding.");
+    }
 
 	do {
+        //TODO: check if this is the final stage message from this transaction
 		checkMessage(readlen, incomingMessage, sizeof(incomingMessage));
 		std::string finalResponse = incomingMessage;
-		cout << "FINAL RESPONSE: " << finalResponse << "\n";
+        // cout << "FINAL RESPONSE: " << finalResponse << "\n";
+
+        //Check and make sure the final ack from server is the same from our last message on
+        // "a.txt/SUCCESS" of "a.txt/FAILURE"
 		if(finalResponse == msg) return true;
-                readlen = sock -> read(incomingMessage,
-				sizeof(incomingMessage));
+        readlen = sock -> read(incomingMessage, sizeof(incomingMessage));
 	}while(!sock->timedout());
+
 	return false;		
 } 
--- fileserver.cpp
+++ fileserver.cpp
@@ -14,19 +14,23 @@
 #include <cstdlib>
 #include <openssl/sha.h>
 
-
+using namespace std;
 using namespace C150NETWORK;
+
+const int netNastinessArg = 1;
+const int fileNastinessArg = 2;
+const int targetDirArg = 3;
 
 void checkDirectory(char *dirname);
 string checkFiles(C150DgmSocket *sock, char *targetDir, string fileName);
 string computeSha(string fileName, char *targetDir);
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+    GRADEME(argc, argv);
 	ssize_t readlen;
 	char incomingMessage[512];
-	int networkNastiness;
+
 	if(argc != 4){
 		fprintf(stderr, "Correct syntax is: %s <networknastiness> <filenastiness> <target_directory>\n", argv[0]);
 			exit(1);	
@@ -36,45 +40,49 @@ main(int argc, char *argv[])
 		fprintf(stderr, "Correct syntax is: %s <nastiness_number>\n", argv[0]);
 		exit(4);
 	}
-	networkNastiness = atoi(argv[1]);
+	int networkNastiness = atoi(argv[netNastinessArg]);
 	try {
 		C150DgmSocket *sock = new C150NastyDgmSocket(networkNastiness);
 		while(1){
+
+            //TODO: inconsistent hanlding of incoming messages
 			readlen = sock -> read(incomingMessage, sizeof(incomingMessage)-1);
-			if (readlen ==0) {
+			if (readlen == 0) {
 				continue;
 			}
 			incomingMessage[readlen] = '\0';
 			string incoming(incomingMessage);
+            //TODO: add possible checkMessage function, and check transaction ID
 			cleanString(incoming);
-			cout << "incoming: " << incoming << "\n";
-			printf("incomingMessage: %s", incomingMessage);
+			// cout << "incoming: " << incoming << "\n";
+			// printf("incomingMessage: %s", incomingMessage);
+
+            //Assume the messag is not first stage message, which only has file without any flag
 			string flag = "";
 			bool isflag = true;
 			for(int i = incoming.length() - 1; i >= 0; i--){
 				if(incoming[i] == '/') isflag= false;
 				if(isflag) flag += incoming[i];
-				else break;
+				else break; //Finished getting the all flag
 			}
-                        if (flag ==  "SSECCUS" || flag == "ERULIAF")
-                        {
-                                string finalAck = incoming;
-				cout << "finalAck: " << finalAck << '\n';
-                                sock -> write(finalAck.c_str(),
-                                        finalAck.length()+1);
-                        }
+
+            //Since we read backwards, the checking string is backward. Will improve in later version
+            if (flag ==  "SSECCUS" || flag == "ERULIAF"){
+				// cout << "finalAck: " << incoming << '\n';
+                if (flag ==  "SSECCUS") *GRADING << "File: " << incoming << " end-to-end check succeeded" << endl;
+                if (flag ==  "ERULIAF") *GRADING << "File: " << incoming << " end-to-end check failed" << endl;
+                sock -> write(incoming.c_str(), incoming.length()+1);
+            }
+            //If this is not a message with flag, this is a request to check new file sha1
 			else
 			{
-				string response = incoming + '/' + 
-						checkFiles(sock,
-						argv[3], incoming);
-				sock -> write(response.c_str(),
-						 response.length()+1);
+                //TODO: add process for accepting the entire file
+                *GRADING << "File: " << incoming << " starting to receive file" << endl;
+                //TODO: pass filenastiness
+				string response = incoming + '/' + checkFiles(sock, argv[targetDirArg], incoming);
+				sock -> write(response.c_str(), response.length()+1);
 			}
-			//readlen = sock -> read(incomingMessage, sizeof(incomingMessage)-1);
-			//fprintf(stderr, "Incoming: %s", incomingMessage);
-			//cout << incoming << '\n';
-			
+			//TODO: the current version only works in nastiness 0 and 1
 		}
 	}
 	catch(C150NetworkException e){
@@ -84,45 +92,38 @@ main(int argc, char *argv[])
 
 string checkFiles(C150DgmSocket* sock, char *targetDir, string fileName)
 {
+    *GRADING << "File: " << fileName << " received, beginning end-to-end check" << endl;
 	checkDirectory(targetDir);
 	DIR *TARGET;
-//	struct dirent *sourceFile;
 	TARGET = opendir(targetDir);
 	if (TARGET == NULL) {
    		fprintf(stderr,"Error opening source directory %s\n", 
 						targetDir);
    		exit(8);
  	}
-  //	while ((sourceFile = readdir(TARGET)) != NULL) {
-   		// skip the . and .. names
-       // 	if ((strcmp(sourceFile->d_name, ".") == 0) ||
-       //     	    (strcmp(sourceFile->d_name, "..")  == 0 ))
-       //   	continue;          // never copy . or ..
-    
-       	   	 // do the copy -- this will check for and
-        	 // skip subdirectories
-        string response = computeSha(fileName, targetDir);
-     //	}
-        closedir(TARGET);
+
+    string response = computeSha(fileName, targetDir);
+    closedir(TARGET);
 	return response;
 }
 
-void
-checkDirectory(char *dirname) {
-  struct stat statbuf;
-  if (lstat(dirname, &statbuf) != 0) {
-    fprintf(stderr,"Error stating supplied source directory %s\n", dirname);
-    exit(8);
-  }
+void checkDirectory(char *dirname) {
+    struct stat statbuf;
+    if (lstat(dirname, &statbuf) != 0) {
+        fprintf(stderr,"Error stating supplied source directory %s\n", dirname);
+        exit(8);
+    }
 
-  if (!S_ISDIR(statbuf.st_mode)) {
-    fprintf(stderr,"File %s exists but is not a directory\n", dirname);
-    exit(8);
-  }
+    if (!S_ISDIR(statbuf.st_mode)) {
+        fprintf(stderr,"File %s exists but is not a directory\n", dirname);
+        exit(8);
+    }
 }
 
 string computeSha(string fileName, char *targetDir)
 {
+    //TODO: this current version do not use nastyfile due to our previous bug
+    // we will definitely fix in final submission  
 	char sha1hex[40];
 	std::string path = targetDir;
 	//add '/' to end of path if it is not there
@@ -130,7 +131,7 @@ string computeSha(string fileName, char *targetDir)
 	ifstream *t;
 	stringstream *buffer;
 	unsigned char obuf[20];
-	printf ("SHA1 (\"%s\") = ", (path+fileName).c_str());
+	// printf ("SHA1 (\"%s\") = ", (path+fileName).c_str());
 	t = new ifstream((path+fileName).c_str());
 	buffer = new stringstream;
 	*buffer << t->rdbuf();
