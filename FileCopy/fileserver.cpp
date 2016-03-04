f3ce5ef96ea49c90f4fe0c6663dbba7fb27bab3e
Richard Zhang <richard.zhang711@gmail.com>
2016-03-03 21:12:31 -0500

    server commented

--- FileCopy/fileserver.cpp
+++ FileCopy/fileserver.cpp
@@ -1,4 +1,4 @@
-#include "c150nastyfile.h"        // for c150nastyfile & framework
+#include "c150nastyfile.h"
 #include "c150grading.h"
 #include "c150nastydgmsocket.h"
 #include "c150debug.h"
@@ -6,10 +6,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <cstring>                // for errno string formatting
+#include <cstring>
 #include <cerrno>
-#include <cstring>               // for strerro
-#include <iostream>               // for cout
+#include <cstring>
+#include <iostream>
 #include <fstream>
 #include <cstdlib>
 #include <openssl/sha.h>
@@ -38,19 +38,18 @@ struct currentFile{
     bool readyForNew; //are we ready for a new file
 };
 
+void listen(char* argv[], C150DgmSocket *sock, int fileNastiness, 
+        currentFile *file, NASTYFILE *outputfile);
 void checkDirectory(char *dirname);
 string checkFiles(C150DgmSocket *sock, char *targetDir, string fileName, int fileNastiness);
 string computeSha(string fileName, char *targetDir, int fileNastiness);
-void listen(char* argv[], C150DgmSocket *sock, int fileNastiness, 
-		currentFile *file, NASTYFILE *outputfile);
-void makePacket(packetStruct *packet, char flag, char ackFlag, uint32_t fileNum, 		uint32_t fileOffset, const char* data);
+void makePacket(packetStruct *packet, char flag, char ackFlag, uint32_t fileNum, uint32_t fileOffset, const char* data);
 void writeData(packetStruct packet, char *argv[], int fileNastiness, 
 		currentFile *file, NASTYFILE *outputFile);
 bool renameTemp(packetStruct packet, char *argv[]);
 
 int main(int argc, char *argv[])
 {
-    cout << sizeof(packetStruct) << endl;
     GRADEME(argc, argv);
 
     if(argc != 4){
@@ -62,21 +61,21 @@ int main(int argc, char *argv[])
 	fprintf(stderr, "Correct syntax is: %s <nastiness_number>\n", argv[0]);
 	exit(4);
     }
+
     int networkNastiness = atoi(argv[netNastinessArg]);
     int fileNastiness = atoi(argv[fileNastinessArg]);
-    try {
-	C150DgmSocket *sock = new C150NastyDgmSocket(networkNastiness);
-	currentFile file;
-	//sock -> turnOnTimeouts(300);
 
-	file.readyForNew = true;
-	file.renamed = false;
-	NASTYFILE outputFile(fileNastiness);
-	while(1){
+    try {
+    	C150DgmSocket *sock = new C150NastyDgmSocket(networkNastiness);
+    	currentFile file;
+
+    	file.readyForNew = true;
+    	file.renamed = false;
+    	NASTYFILE outputFile(fileNastiness);
+    	while(1){
             listen(argv, sock, fileNastiness, &file, &outputFile);
             //TODO: inconsistent hanlding of incoming messages
-			//TODO: the current version only works in nastiness 0 and 1
-	}
+	   }
     }
     catch(C150NetworkException e){
 	cerr << argv[0] << ": caught C150NetworkException: " << e.formattedExplanation() << endl;		
@@ -93,122 +92,90 @@ void listen(char * argv[], C150DgmSocket *sock,  int fileNastiness,
         return;
     }
     incomingMessage[readlen] = '\0';
-//    cout << incomingMessage << " <- sock message " << endl;
-    //string incoming(incomingMessage);
-    //TODO: add possible checkMessage function, and check transaction ID
-    //cleanString(incoming);
-    // cout << "incoming: " << incoming << "\n";
-    // printf("incomingMessage: %s", incomingMessage); 
     packetStruct incomingPacket;
     memcpy(&incomingPacket, incomingMessage, sizeof(incomingMessage));
 
-    //Assume the messag is not first stage message, which only has file without any flag
-
-    //Since we read backwards, the checking string is backward. Will improve in later version
     if(incomingPacket.flag == 'S'){ //if this is a start packet
-        cout << "START PACKET RECEIVED FOR FILE: " << incomingPacket.data << endl;
         if(!(file -> readyForNew)){
-	    if(incomingPacket.fileNum == file -> fileNum){
-	        packetStruct responsePacket;
-        	makePacket(&responsePacket, 'A', 'S', incomingPacket.fileNum, 0, 0); //A for ack, S for start
-        	char msgBuffer[sizeof(responsePacket)];
-        	memcpy(msgBuffer, &responsePacket, sizeof(responsePacket));
-		sock -> write(msgBuffer, sizeof(msgBuffer) + 8);
-	    }
-	    return;
-	}
-	file -> renamed = false;
-	file -> fileNum = incomingPacket.fileNum;
-	strcpy(file -> fileName, incomingPacket.data);
-	file -> readyForNew = false;
-	printf("filename: %s", file -> fileName);
-	std::string targetDir = argv[targetDirArg];
-	if(targetDir[targetDir.length() - 1] != '/') targetDir += '/';
-    	std::string fileName = file -> fileName;
-    	string path = targetDir + fileName + ".TMP";
-	void *fopenretval;
-	fopenretval = outputFile->fopen(path.c_str(), "wb+"); //open for read and write binary
-	if (fopenretval == NULL) {
+    	    if(incomingPacket.fileNum == file -> fileNum){
+                *GRADING << "File " << file->fileName << " starting to receive file" << endl;
+    	        packetStruct responsePacket;
+            	makePacket(&responsePacket, 'A', 'S', incomingPacket.fileNum, 0, 0); //A for ack, S for start
+            	char msgBuffer[sizeof(responsePacket)];
+            	memcpy(msgBuffer, &responsePacket, sizeof(responsePacket));
+    		    sock -> write(msgBuffer, sizeof(msgBuffer) + 8);
+	       }
+	       return;
+        }
+        file -> renamed = false;
+        file -> fileNum = incomingPacket.fileNum;
+        strcpy(file -> fileName, incomingPacket.data);
+        file -> readyForNew = false;
+        *GRADING << "File " << file->fileName << " starting to receive file" << endl;
+        
+        std::string targetDir = argv[targetDirArg];
+        if(targetDir[targetDir.length() - 1] != '/') targetDir += '/';
+        
+        std::string fileName = file -> fileName;
+        string path = targetDir + fileName + ".TMP";
+        
+        void *fopenretval;
+        fopenretval = outputFile->fopen(path.c_str(), "wb+"); //open for read and write binary
+        if (fopenretval == NULL) {
             cerr << "Error opening input file " << path << "errno=" <<
                 strerror(errno) << endl;
             exit(12);
-    	}
-	 
-	packetStruct responsePacket;
+        }
+        packetStruct responsePacket;
         makePacket(&responsePacket, 'A', 'S', incomingPacket.fileNum, 0, 0); //A for ack, S for start
-	char msgBuffer[sizeof(responsePacket)];
-	memcpy(msgBuffer, &responsePacket, sizeof(responsePacket));
-	//int temp_AS = 0;
-	//do{
-	//    temp_AS++;
-	sock -> write(msgBuffer, sizeof(msgBuffer) + 8);
-	//}while(!sock->timedout()&& temp_AS < 5);
-	
+        char msgBuffer[sizeof(responsePacket)];
+        memcpy(msgBuffer, &responsePacket, sizeof(responsePacket));
+        sock -> write(msgBuffer, sizeof(msgBuffer) + 8);	
     }
-    else if(incomingPacket.flag == 'E'){
-        cout << "END PACKET RECIEVED FOR FILE: " << incomingPacket.data 
-        << "---computing checksum" << endl;
-	if(!file -> readyForNew){
-	    if (outputFile->fclose() != 0){
-                cerr << "Error closing output file " <<
-                    " errno=" << strerror(errno) << endl;
-                exit(16);
+    else if(incomingPacket.flag == 'E'){//if this is the end of file
+    	if(!file -> readyForNew){
+    	    if (outputFile->fclose() != 0){
+                    cerr << "Error closing output file " <<
+                        " errno=" << strerror(errno) << endl;
+                    exit(16);
             }
-	    file -> readyForNew = true;
-	}
+    	    file -> readyForNew = true;
+    	}
         string sha = checkFiles(sock, argv[targetDirArg], incomingPacket.data, fileNastiness);
         packetStruct responsePacket;
-        makePacket(&responsePacket, 'C', 0, incomingPacket.fileNum, 0,
-			 sha.c_str());
+        makePacket(&responsePacket, 'C', 0, incomingPacket.fileNum, 0, sha.c_str());
         char msgBuffer[sizeof(responsePacket)];
         memcpy(msgBuffer, &responsePacket, sizeof(responsePacket));
         sock -> write(msgBuffer, sizeof(msgBuffer) + 8);
     }
-    else if(incomingPacket.flag == 'C'){
-	cout << "END-TO-END RESULTS RECEIVED--RESULT: " 
-	<< incomingPacket.data << endl;
-	//TODO: if rename is false then have client resend file
-	if(!(file -> renamed)){
-		if(renameTemp(incomingPacket, argv)){
-			file -> renamed = true;
-		}
-	}
-	packetStruct responsePacket;
-	makePacket(&responsePacket, 'A', 'C', incomingPacket.fileNum, 0, 0);
-	char msgBuffer[sizeof(responsePacket)];
-	memcpy(msgBuffer, &responsePacket, sizeof(responsePacket));
-	sock -> write(msgBuffer, sizeof(msgBuffer) + 8);
-	file -> readyForNew = true;
+    else if(incomingPacket.flag == 'C'){//if this contains sha1 check result
+        *GRADING << "File " << file->fileName << " end-to-end check " << incomingPacket.data << endl;
+    	if(!(file -> renamed)){
+    		if(renameTemp(incomingPacket, argv)){
+    			file -> renamed = true;
+    		}
+    	}
+    	packetStruct responsePacket;
+    	makePacket(&responsePacket, 'A', 'C', incomingPacket.fileNum, 0, 0);
+    	char msgBuffer[sizeof(responsePacket)];
+    	memcpy(msgBuffer, &responsePacket, sizeof(responsePacket));
+    	sock -> write(msgBuffer, sizeof(msgBuffer) + 8);
+    	file -> readyForNew = true;
     }
-    else if(incomingPacket.flag == 'D'){
-	//cout << "DATA PACKET RECEIVED" << endl;
-	writeData(incomingPacket, argv, fileNastiness, file, outputFile);
-	packetStruct responsePacket;
-	makePacket(&responsePacket, 'A', 'D', incomingPacket.fileNum,
-		incomingPacket.fileOffset, 0);
-	char msgBuffer[sizeof(responsePacket)];
-	memcpy(msgBuffer, &responsePacket, sizeof(responsePacket));
-	sock -> write(msgBuffer, sizeof(msgBuffer) + 8);
-    } 
-   /* if (flag ==  "SSECCUS" || flag == "ERULIAF"){
-        // cout << "finalAck: " << incoming << '\n';
-        if (flag ==  "SSECCUS") *GRADING << "File: " << incoming << " end-to-end check succeeded" << endl;
-        if (flag ==  "ERULIAF") *GRADING << "File: " << incoming << " end-to-end check failed" << endl;
-        sock -> write(incoming.c_str(), incoming.length()+1);
+    else if(incomingPacket.flag == 'D'){//if this is a data packet for file
+    	writeData(incomingPacket, argv, fileNastiness, file, outputFile);
+    	packetStruct responsePacket;
+    	makePacket(&responsePacket, 'A', 'D', incomingPacket.fileNum,
+    		incomingPacket.fileOffset, 0);
+    	char msgBuffer[sizeof(responsePacket)];
+    	memcpy(msgBuffer, &responsePacket, sizeof(responsePacket));
+    	sock -> write(msgBuffer, sizeof(msgBuffer) + 8);
     }
-    //If this is not a message with flag, this is a request to check new file sha1
-    else
-    {
-        //TODO: add process for accepting the entire file
-        *GRADING << "File: " << incoming << " starting to receive file" << endl;
-        //TODO: pass filenastiness
-        string response = incoming + '/' + checkFiles(sock, argv[targetDirArg], incoming, fileNastiness);
-        sock -> write(response.c_str(), response.length()+1);
-    }*/
 }
 
 bool renameTemp(packetStruct packet, char *argv[])
 {
+    // for renaming .TMP suffix
 	std::string data = packet.data;
 	string fileName = "";
 	string result = "";
@@ -243,36 +210,11 @@ void writeData(packetStruct packet, char *argv[], int fileNastiness,
 		currentFile *file, NASTYFILE *outputFile)
 {
     size_t len;
-//    NASTYFILE outputFile(fileNastiness);
-//    void *fopenretval;
-//    std::string targetDir = argv[targetDirArg];
-//    printf("filename when writing: %s", file ->fileName);
-    //add '/' to end of path if it is not there
-/*    if(targetDir[targetDir.length() - 1] != '/') targetDir += '/';
-    std::string fileName = file -> fileName;
-    string path = targetDir + fileName;
-    fopenretval = outputFile.fopen(path.c_str(), "wb+"); //open for read and write binary
-    if (fopenretval == NULL) {
-	cerr << "Error opening input file " << path << "errno=" <<
-		strerror(errno) << endl;
-	exit(12);
-    }
-*/
     outputFile->fseek((packet.fileOffset * MAX_DATA_BYTES), SEEK_SET);
-    //cout << "offset: " << packet.fileOffset << endl;
-    //printf("Data: %s\n", packet.data);
     len = outputFile->fwrite(packet.data, 1, packet.numBytes);
     if(len != packet.numBytes){
 	cerr << "error when writing" << endl;
     }
-/*
-    if (outputFile.fclose() != 0){
-        cerr << "Error closing output file " << path <<
-            " errno=" << strerror(errno) << endl;
-        exit(16);
-    }
-  */  
-    
 }
 void makePacket(packetStruct *packet, char flag, char ackFlag, uint32_t fileNum, 
                 uint32_t fileOffset, const char* data)
@@ -330,25 +272,21 @@ string computeSha(string fileName, char *targetDir, int fileNastiness)
     //add '/' to end of path if it is not there
     if(path[path.length() - 1] != '/') path += '/';
     string tempPath = path + fileName + ".TMP";
-    // const char *full_path = (path+fileName).c_str();
-    printf ("SHA1 (\"%s\") is computed\n ", tempPath.c_str());
-     
-       
-    fopenretval = inputFile.fopen(tempPath.c_str(), "rb");
 
+    fopenretval = inputFile.fopen(tempPath.c_str(), "rb");
 
     if (fopenretval == NULL) {
         cerr << "Error opening input file " << (path+fileName).c_str() << 
             " errno=" << strerror(errno) << endl;
         exit(12);
     }
+
     inputFile.fseek(0, SEEK_END);
     sourceSize = inputFile.ftell();
     buffer = (char *)malloc(sourceSize);
     if(buffer == NULL) exit(1);
     inputFile.fseek(0, SEEK_SET);
     len = inputFile.fread(buffer, 1, sourceSize);
-    cout << sourceSize << " " << sizeof(*buffer) << endl;
 
     if (len != sourceSize) {
       cerr << "Error reading file " << (path+fileName).c_str() << 
@@ -364,10 +302,8 @@ string computeSha(string fileName, char *targetDir, int fileNastiness)
     SHA1((const unsigned char *)buffer, len, obuf);
     for (int i = 0; i < 20; i++)
     {
-	printf( "%02x", (unsigned int) obuf[i]);
         sprintf (sha1hex + (i*2), "%02x", (unsigned int) obuf[i]);
     }
-    printf ("\n");
 
     free(buffer);
 
